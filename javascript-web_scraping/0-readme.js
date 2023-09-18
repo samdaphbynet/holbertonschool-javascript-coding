650044b251e63853737ea1649db63bee7ab5c64a
samdaphbynet <lj8@hotmail.fr>
2023-09-18 15:31:39 +0000

    delete documentation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-// Write a script that reads and prints the content of a file.
-
-// - The first argument is the file path
-// - The content of the file must be read in utf-8
-// - If an error occurred during the reading, print the error object
-
 
 const fs = require('fs');
-
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
