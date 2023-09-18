de482412242427958956f56948ede301dac41c49
samdaphbynet <lj8@hotmail.fr>
2023-09-18 16:17:25 +0000

    script that display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Make sure you have the "request" module installed. If you don't have it installed, you can install it using npm:
+
+// npm install request
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.log(error.message);
+    }
+    console.log(`code: ${response.statusCode}`);
+})
