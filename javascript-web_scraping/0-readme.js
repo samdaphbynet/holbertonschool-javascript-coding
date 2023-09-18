a7b2db5a1839780a5aebaf268bbb036afc4fddf8
samdaphbynet <lj8@hotmail.fr>
2023-09-18 15:33:24 +0000

    delete else

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
+    if (err) {
     console.error(err);
     return;
-  } else {
+    } 
     console.log(data);
-  }
 });
