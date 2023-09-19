8dd0f566d0346d961765d11106848b3b12634c76
samdaphbynet <lj8@hotmail.fr>
2023-09-19 11:41:15 +0000

    script that gets the contents of a webpage and stores it in a file.

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const request = require('request');
+
+const url = process.argv[2];
+
+const arg2 = process.argv[3];
+
+request(url, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        fs.writeFile(arg2, body, (err) => {
+            if (err) throw err;
+        })
+    }
+})
