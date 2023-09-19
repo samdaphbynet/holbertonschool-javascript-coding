1d533b83e08fd83c4e8ea3638890e48aabe86e95
samdaphbynet <lj8@hotmail.fr>
2023-09-19 11:24:38 +0000

    added function countChar18()

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,29 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 
 const apiUrl = process.argv[2];
-const characterId = 20;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error.message);
   } else {
-      const films = JSON.parse(body).results;
-      const count = films.filter((film) =>
-        film.characters.some((character) => character.endsWith(`/${characterId}/`))
-      ).length;
-      console.log(count);
-    
+    const data = JSON.parse(body);
+    const count = countChar18(data.results);
+    console.log(count);
   }
 });
+
+function countChar18(results) {
+  let count = 0;
+  for (const result of results) {
+    if (result.characters) {
+      for (const character of result.characters) {
+        if (character.includes("18")) {
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+}
