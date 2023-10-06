3b9c0602b27308b299c6d4963664e7aad9ff0315
samdaphbynet <lj8@hotmail.fr>
2023-10-06 12:11:21 +0000

    translate character to upper case

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
-  console.log(`your name is: ${input}`);
+  console.log(`Your name is: ${input}`);
 });
 
 rl.on('close', () => {
