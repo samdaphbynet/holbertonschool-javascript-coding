7801f0f22fedef09f88eb37d3c692201a2f7a323
samdaphbynet <lj8@hotmail.fr>
2023-10-09 15:32:16 +0000

    update function

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
-    const students = lines.slice(1).filter(line => line).map(line => line.split(','));
+    const students = lines.slice(1).filter((line) => line).map((line) => line.split(','));
     const fields = {};
 
     for (const student of students) {
@@ -19,7 +19,9 @@ function countStudents(path) {
 
     console.log(`Number of students: ${students.length}`);
     for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
     }
   } catch (err) {
     throw new Error('Cannot load the database');
